08037f4e0577608ee19dbd308881410e1f486f40
Daniel Muller <DMuller@DM.cfl.rr.com>
2017-09-16 15:38:19 -0400

    Updated Started Year Format

--- script.js
+++ script.js
@@ -52,50 +52,18 @@
 
 
       // Prettify the employee start
-      var tStartPretty = moment.unix(tStart).format("MM/DD/YY");
+      var tStartPretty = moment.unix(tStart).format("MM/DD/YYYY");
+      console.log("tStart: " + tStart);
       console.log("Pretty Employee Start Date: " + tStartPretty);
-
       // Calculate the months worked using hardcore math
       // To calculate the months worked
       var empMonths = moment().diff(moment.unix(tStart, "X"), "months");
       console.log("Months worked: " + empMonths);
-
       // Calculate the total billed rate
       var empBilled = empMonths * tRate;
       console.log(empBilled);
-
       // Add each train's data into the table
       $(".schedTable").append("<tr><td>" + tName + "</td><td>" + tRole + "</td><td>" +
       tStartPretty + "</td><td>" + empMonths + "</td><td>" + tRate + "</td><td>" + "$" + empBilled + "</td></tr>");
 
-
-
-//GRABBED FROM EXERCISE IN CLASS:
-      // Assumptions
-          // var tFrequency = 3;
-          // // Time is 3:30 AM
-          // var firstTime = "03:30";
-          // // First Time (pushed back 1 year to make sure it comes before current time)
-          // var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
-          // console.log(firstTimeConverted);
-          //
-          // // Current Time
-          // var currentTime = moment();
-          // console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-          //
-          // // Difference between the times
-          // var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
-          // console.log("DIFFERENCE IN TIME: " + diffTime);
-          //
-          // // Time apart (remainder)
-          // var tRemainder = diffTime % tFrequency;
-          // console.log(tRemainder);
-          //
-          // // Minute Until Train
-          // var tMinutesTillTrain = tFrequency - tRemainder;
-          // console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
-          //
-          // // Next Train
-          // var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-          // console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
   })
